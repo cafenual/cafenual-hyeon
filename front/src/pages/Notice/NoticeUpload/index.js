0b815f273a2723ab9b76a4d0500c27f7d282f6ab
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-06-18 22:17:44 +0900

    Style : NoticeUpload UI

---
+++ front/src/pages/Notice/NoticeUpload/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles.css";
+import Header from "layouts/Header";
+
+const NoticeUpload = () => {
+  return (
+    <div id="NoticeUpload">
+      <Header />
+      <div className="back-ground">
+        <div className="upload-form">
+          <form action="">
+            <div className="upload-title">
+              <input
+                type="text"
+                className="input-title"
+                placeholder="제목을 입력하세요"
+              />
+            </div>
+            <div className="upload-cont"></div>
+            <div className="upload-btn">
+              <button type="submit" className="cont-upload">
+                등록하기
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoticeUpload;
