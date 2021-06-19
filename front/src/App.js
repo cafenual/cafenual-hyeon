f94908f81e07e72859b57e39db3b8ebdefc3c2ff
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-06-20 00:34:02 +0900

    Style : NoticeDetail UI

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,10 @@
 import Commute from "pages/Commute";
 import Join from "pages/Join";
 import Login from "pages/Login";
-import DashBoard from "pages/DashBoard"
+import DashBoard from "pages/DashBoard";
+import NoticeList from "pages/Notice/NoticeList";
+import NoticeUpload from "pages/Notice/NoticeUpload";
+import NoticeDetail from "pages/Notice/NoticeDetail";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -12,9 +15,13 @@ function App() {
         <div id="main">
           <Switch>
             <Route path="/dashboard" exact component={DashBoard} />
-            <Route path="/" exact component={Login} /> {/* exact를 안치면 정확하게가 아니어서 다른 페이지에 /가 붙어 있으므로 다른 페이지로 이동이 안됨 */}
+            <Route path="/" exact component={Login} />{" "}
+            {/* exact를 안치면 정확하게가 아니어서 다른 페이지에 /가 붙어 있으므로 다른 페이지로 이동이 안됨 */}
             <Route path="/commute" component={Commute} />
             <Route path="/join" component={Join} />
+            <Route path="/notice" exact component={NoticeList} />
+            <Route path="/notice/upload" exact component={NoticeUpload} />
+            <Route path="/notice/detail" component={NoticeDetail} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ front/src/pages/Notice/NoticeDetail/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./styles.css";
+import Header from "layouts/Header";
+
+const NoticeDetail = () => {
+  return (
+    <div id="NoticeDetail">
+      <Header />
+      <div className="back-ground">
+        <div className="detail-form">
+          <div className="detail-top">
+            <div className="detail-title">title</div>
+            <div className="title-bottom">
+              <div className="detail-date">0000-00-00</div>
+              <a href="/notice">목록</a>
+            </div>
+          </div>
+          <div className="datail-middle">
+            <div className="detail-cont">content</div>
+          </div>
+          <div className="datail-bottom">
+            <div className="bottom-left">
+              <a href="">이전</a>
+              <a href="">다음</a>
+            </div>
+            <div className="bottom-right">
+              <a href="/notice/edit">수정</a>
+              <a href="">삭제</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoticeDetail;
