c36826f5e4121b970e4692d3c353c0b31f85cffb
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-06-30 23:24:58 +0900

    Test : mongodb 비밀번호 바꿈

--- back/index.js
+++ back/index.js
@@ -6,14 +6,14 @@ const app = express(); //express 실행시킴
 
 mongoose
   .connect(
-    "mongodb+srv://mag3010:min893010@@cafenual.8soqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://mag3010:hyeon3010@cafenual.8soqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
   .then(() => console.log("DB 실행됨")) //DB가 잘 실행되고 있다면
-  .catch((err) => console.log(err));    //에러가 잡히면 이쪽으로
+  .catch((err) => console.log(err)); //에러가 잡히면 이쪽으로
 
 const handleListening = () => {
   console.log("서버가 시작됨 : http://localhost:4000"); //잘실행됬는지 확인하기 위해서
