c76b9b69d136b288d9c0abaa9651e113c11e515a
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-07-13 20:50:19 +0900

    Implements : Register API

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -2,18 +2,14 @@ import User from "../models/user";
 
 //회원가입
 export const register = async (req, res) => {
-  const { email, password, name } = req.body;
+  const { email, password, name } = req.body; //이렇게 하면 req.body.email하지 않고 email로 사용할 수 있다
 
   try {
     //이메일이 디비에 있는지 확인
     const exist = await User.findOne({
-      email,
-    });
-
-    const user = new User({
-      email, //email = email과 같은 문법
-      password,
-      name,
+      //fineOne은 결과 값이 true 아니면 false가 나옴
+      //밑에 있는 작업과 같이 하면 무조건 없다고 뜰때니 할때까지 기달려라 await
+      email, //email = email
     });
 
     if (exist) {
@@ -24,8 +20,16 @@ export const register = async (req, res) => {
       });
     }
 
+    const user = new User({
+      email, //email = email과 같은 문법
+      password,
+      name,
+    });
+
+    await user.setPassword(password); //암호화
     await user.save();
     //await->기달려라, 없어도 저장되기 하는데 저장되는 와중에 밑에문이 실행되서 undefine이 될수 있음
+    //DB 저장해라
 
     res.status(200).json({
       //성공했다
@@ -33,6 +37,7 @@ export const register = async (req, res) => {
       user,
     });
   } catch (e) {
+    //에러 캐치
     res.status(400).json({
       //res -> front에게 보내주겠다
       success: false,
--- back/models/user.js
+++ back/models/user.js
@@ -1,25 +1,62 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    require: true, //무조건 입력이 되어야 한다
-    unique: true, //무조건 유니크하다 같은게 있으면 오류
-    trim: true, //공백을 없애주겠다
-    lowercase: true, //대문자로 해도 다 소문자로 받겠다
-  },
-  name: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    require: true,
-    trim: true,
-    minLength: 6, //최소 6글자
-  },
-});
+const saltRounds = 10; //hash를 10번 하겠다. 1번만 하면 해커가 뚫을수 있으니 여러번 해주는것
+//상수화를 해놓는것이 좋다 상수화를 변수를 넣어줘야 유지보수에 좋기 때문
 
-const User = mongoose.model("User", UserSchema);    //mongodb언어로 변화 이름은 User라는 이름으로
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      require: true, //무조건 입력이 되어야 한다
+      unique: true, //무조건 유니크하다 같은게 있으면 오류
+      trim: true, //공백을 없애주겠다
+      lowercase: true, //대문자로 해도 다 소문자로 받겠다
+    },
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+      minLength: 6, //최소 6글자
+    },
+    wage: {
+      type: Number,
+      trim: true,
+      default: 8720, //기본값
+    },
+    status: {
+      type: String,
+      enum: ["재직자", "퇴직자"], //자바스크립트 문법, 조건을 여러개 가질 수 있음
+      default: "재직자",
+    },
+    role: {
+      type: String,
+      enum: ["admin", "parttime"],
+      default: "parttime",
+    },
+    token: {
+      //원래 웹은 새로고침하면 다 사라져서 새로고침할때마다 데이터가 없는데 토큰을 줘서(방문증을 줌) 계속 로그인상태가 되어있도록
+      type: String,
+    },
+  },
+  { timestamps: true }  //최초로 만든시간과 수정한 시간이 나타나서 DB관리에 용이하다
+);
+
+//몽고db에 메서드 만드는 방식
+//1.UserSchema.methods.함수이름 = 함수  =>인스턴스 메서드 : 모델을 통해 만든 문서 인스턴스에서 사용할 수 있는 함수
+//2.UserSchema.statics.함수이름 = 힘수  =>스태틱 메서드 : 모델에서 바로 사용할 수 있는 함수를 의미
+
+UserSchema.methods.setPassword = async function (password) {
+  //이건 비동기함수야 라고 말해주는 함수 async를 쓰면 await을 안써도 되서 이걸 주로 쓴다
+  //this는 함수안에서 할수 있기 때문에 화살표함수를 안쓰고 this 사용
+  const result = await bcrypt.hash(password, saltRounds);
+  this.password = result; //hash한 결과물을 password에 넣어줌
+};
+
+const User = mongoose.model("User", UserSchema); //mongodb언어로 변화 이름은 User라는 이름으로
 export default User;
