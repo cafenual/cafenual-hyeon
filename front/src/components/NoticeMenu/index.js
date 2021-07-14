3b65e7df1b85123c94a840027b68bbec1f96d0b4
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-07-14 23:05:16 +0900

    Style : NoticeMenu UI

---
+++ front/src/components/NoticeMenu/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./styles.css";
+import { NavLink } from "react-router-dom";
+import { NoticeMenuData } from "static/FakeDates";
+
+import "./styles.css";
+
+const FakeNoticeMenuData = NoticeMenuData;
+
+const NoticeMenu = () => {
+  const activeStyle = {
+    color: "#424242",
+    fontSize: "18px",
+  };
+
+  return (
+    <div id="NoticeMenu">
+      <div className="list-block">
+        <ul className="list">
+          {FakeNoticeMenuData.map((data, index) => (
+            <li className="list-left" key={index}>
+              <NavLink
+                activeStyle={activeStyle}
+                exact
+                to={data.name === "notice" ? "/notice" : `/notice/${data.name}`}
+              >
+                {data.text}
+              </NavLink>
+            </li>
+          ))}
+          <li className="list-right">
+            <a href="/notice/categoryedit">목록수정</a>
+            <a href="/notice/upload">등록</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NoticeMenu;
