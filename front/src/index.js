0edc39568af9e28658478445fed27c3df64b6ad2
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-08-17 22:34:35 +0900

    Implements : 로그인 상태 유지, 로그아웃 구현

--- front/src/index.js
+++ front/src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "modules";
+import { Provider } from "react-redux";
+import { SetUser } from "modules/users";
+
+function loadUser() {
+  try {
+    let user = sessionStorage.getItem("user"); //get잉께 sessionStorage에 user있음 가져오랑께
+    if (!user) return;
+
+    store.dispatch(SetUser(JSON.parse(user))); //들여보낼때 문자열로 넣었으니 꺼낼때는 json으로 꺼내오자 json으로 변형
+  } catch (e) {
+    console.log("logUser 오류");
+  }
+}
+
+loadUser();
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -15,3 +36,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//제일 먼저 렌더링 되는 아이
--- front/src/layouts/Header/index.js
+++ front/src/layouts/Header/index.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom"; //메뉴를 클릭할때 링크를 타고 갈 수 있도록
 import "./styles.css";
 import { BiFoodMenu, BiBell, BiCalendarEdit } from "react-icons/bi";
 import { BsGrid, BsCardList } from "react-icons/bs";
 import cafe from "static/img/cafe.png";
+import axios from "axios";
+import { EmptyUser } from "modules/users";
 
 const Header = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const onLogout = async () => {
+    try {
+      await axios.post("http://localhost:4000/api/user/logout");
+      sessionStorage.removeItem("user"); //로그아웃을 할때 세션에 있는걸 지워줌
+      dispatch(EmptyUser());
+    } catch (e) {
+      alert("로그아웃에 실패했습니다");
+    }
+  };
+
   const activeStyle = {
     backgroundColor: "#FAFAFA",
     color: "#585858",
@@ -61,9 +77,23 @@ const Header = () => {
           <div className="right-nav">
             <ul>
               <li>
-                <a href="/">로그아웃</a>
+                <a href="/" onClick={onLogout}>
+                  로그아웃
+                </a>
               </li>
             </ul>
+            {/* {user.email && (
+              <>
+                <div>{user.name}님 안녕하세요</div>
+                <button onClick={onLogout}>로그아웃</button>
+              </>
+            )}
+            {!user.email && (
+              <>
+                <button>로그인</button>
+                <button>회원가입</button>
+              </>
+            )} */}
           </div>
         </div>
       </div>
